fecb14d1449156cf5631e4a75886f5fb23f8abd0
3dartix <info@3dartix.ru>
2020-08-05 22:29:07 +0300

    test

--- rest-api/src/main/java/com/zbrain/controller/SwaggerController.java
+++ rest-api/src/main/java/com/zbrain/controller/SwaggerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SwaggerController {
     @RequestMapping("/swagger")
+    
     public String home() {
         return "redirect:/swagger-ui.html";
     }
